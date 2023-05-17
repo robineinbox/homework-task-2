76dc285ce0c7f52735e24577ab8e654e97178f79
robineinbox <robin_e@inbox.ru>
2023-05-17 17:14:51 +0300

    everything is working

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,8 +1,8 @@
 from src.channel import Channel
 
 if __name__ == '__main__':
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-    moscowpython.print_info()
+    moscow_python = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    moscow_python.print_info()
 
     """
 {
