bd449b16b570c28b8192707c7be49eccde001e54
robineinbox <robin_e@inbox.ru>
2023-06-01 09:32:58 +0300

    works correctly

--- src/channel.py
+++ src/channel.py
@@ -1,9 +1,109 @@
-class Channel:
-    def __init__(self, channel_id):
-        self.channel_id = channel_id
+import json
+from googleapiclient.discovery import build
 
-    def print_info(self):
-        print(f"Channel ID: {self.channel_id}")
+
+class Channel:
+    """Класс для ютуб-канала"""
+    API_KEY = 'AIzaSyDsBEAxo4P9SfFuKeJImC8jgL9sQXfsbq4'
+
+    def __init__(self, channel_id: str) -> None:
+        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
+        self.title = ''
+        self.description = ''
+        self.url = ''
+        self.subscriber_count = 0
+        self.video_count = 0
+        self.view_count = 0
+
+        # получение соответствующих значений из словаря channel
+        service = self.get_service()
+        channel = service.channels().list(id=channel_id, part='snippet,statistics').execute()
+        if 'items' in channel and len(channel['items']) > 0:
+            snippet = channel['items'][0]['snippet']
+            statistics = channel['items'][0]['statistics']
+            self.title = snippet.get('title', '')
+            self.description = snippet.get('description', '')
+            self.url = 'https://www.youtube.com/channel/' + channel_id
+            self.subscriber_count = int(statistics.get('subscriberCount', 0))
+            self.video_count = int(statistics.get('videoCount', 0))
+            self.view_count = int(statistics.get('viewCount', 0))
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API"""
+        return build('youtube', 'v3', developerKey=cls.API_KEY)
+
+    def to_json(self, filename: str) -> None:
+        """Сохраняет в файл значения атрибутов экземпляра Channel"""
+        data = {
+            'id': self.channel_id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count
+        }
+        with open(filename, 'w') as f:
+            json.dump(data, f)
+
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+        print(f'Title: {self.title}')
+        print(f'Description: {self.description}')
+        print(f'URL: {self.url}')
+        print(f'Subscriber count: {self.subscriber_count}')
+        print(f'Video count: {self.video_count}')
+        print(f'View count: {self.view_count}')
+
+
+if __name__ == '__main__':
+    channel_id = 'YOUR_CHANNEL_ID_HERE'
+    channel = Channel(channel_id)
+    channel.print_info()
+    channel.to_json('channel_info.json')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Channel:
+#     def __init__(self, channel_id):
+#         self.channel_id = channel_id
+#
+#     def print_info(self):
+#         print(f"Channel ID: {self.channel_id}")
 
 
 
